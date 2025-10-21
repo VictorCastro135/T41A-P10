dc243eee0c4c928c648b80704e87406990b7ff8d
VictorCastro135 <178920@upslp.edu.mx>
2025-10-20 19:52:17 -0600

    feature/178920

---
+++ 40_Find_the_total_revenue_of_each_facility.sql
@@ -0,0 +1,9 @@
+SELECT facs.name, SUM(slots * case
+			WHEN memid = 0 then facs.guestcost
+			ELSE facs.membercost
+		END) AS revenue
+	FROM cd.bookings bks
+	INNER JOIN cd.facilities facs
+		ON bks.facid = facs.facid
+	GROUP BY facs.name
+ORDER BY revenue;
